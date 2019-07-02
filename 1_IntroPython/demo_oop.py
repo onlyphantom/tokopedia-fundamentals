a87e325e2ac93c9f387828e8211a57784e9daf91
Samuel Chan <samuel@outlook.sg>
2019-07-02 15:21:46 +0700

    explained instance, class and inheritance

--- 1_IntroPython/demo_oop.py
+++ 1_IntroPython/demo_oop.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Inventory:
     listed = False
 
@@ -24,8 +27,6 @@ class Inventory:
 
     @staticmethod
     def inventory_updated(num_of_days):
-        import datetime
-
         return datetime.datetime.now() - datetime.timedelta(days=num_of_days)
 
 
@@ -41,3 +42,39 @@ print(journal.listed)
 print(Inventory.inventory_updated(2))
 print(Inventory.inventory_updated(3))
 
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.timestamp = datetime.datetime.now()
+
+    def signed_up_on(self):
+        delta = datetime.datetime.now() - self.timestamp
+        secs_since = int(delta.total_seconds())
+        return f"{self.username} signed up {secs_since} seconds ago."
+
+
+class Nakama(User):
+    def __init__(self, username, email, division="Sales"):
+        super().__init__(username, email)
+        self.division = division
+
+    def signed_up_on(self):
+        res = super().signed_up_on()
+        additional = f"{self.username} is a {self.division} employee."
+        return res, additional
+
+
+import time
+
+adam = User(username="kadiB", email="kadi_b@hollywood.com")
+andi = Nakama(username="lauluv2sing", email="andi@tokopedia.com")
+time.sleep(3)
+print(adam.signed_up_on())
+time.sleep(1)
+print(andi.signed_up_on())
+# print(help(Nakama))
+print(isinstance(adam, Nakama))
+print(isinstance(andi, Nakama))
+print(issubclass(Nakama, User))
